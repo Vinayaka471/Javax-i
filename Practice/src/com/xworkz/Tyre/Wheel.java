dae6860747b645b7c4be0c28301e889efff1fde7
vinayaka <vinayakah47@gmail.com>
2025-04-28 07:36:20 +0530

    Adding 3 Numbers

---
+++ Practice/src/com/xworkz/Tyre/Wheel.java
@@ -0,0 +1,10 @@
+package com.xworkz.Tyre;
+
+public class Wheel {
+    public static void main(String[] args){
+        int num1=67;
+        int num2=32;
+        int num3=58;
+        System.out.println(num1+num2+num3);
+    }
+}
