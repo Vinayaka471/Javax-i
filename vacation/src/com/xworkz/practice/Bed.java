548228af1f1976831ad78fbe5aa4a1b154b55642
vinayaka <vinayakah47@gmail.com>
2025-05-07 07:40:26 +0530

    Printing Numbers

---
+++ vacation/src/com/xworkz/practice/Bed.java
@@ -0,0 +1,9 @@
+package com.xworkz.practice;
+
+public class Bed {
+    public static void main(String[] args){
+        for(int i=0;i<101;i++){
+            System.out.println(i);
+        }
+    }
+}
