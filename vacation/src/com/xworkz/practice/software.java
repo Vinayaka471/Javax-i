b80842c1af7d5884cd8b1e74621d6596489ba46b
vinayaka <vinayakah47@gmail.com>
2025-05-09 09:13:17 +0530

    Adding Two Numbers

---
+++ vacation/src/com/xworkz/practice/software.java
@@ -0,0 +1,9 @@
+package com.xworkz.practice;
+
+public class software {
+    public static void main(String[] args){
+        int a=60;
+        int b=308;
+        System.out.println(a+b);
+    }
+}
