792c76afb9e8adb1edbff5f7365bd0c282d8314a
vinayaka <vinayakah47@gmail.com>
2025-05-11 13:56:47 +0530

    Multiplying Two Numbers

---
+++ vacation/src/com/xworkz/practice/Soap.java
@@ -0,0 +1,9 @@
+package com.xworkz.practice;
+
+public class Soap {
+    public static void main(String args[]){
+        int t=35;
+        float q=6.25F;
+        System.out.println(t*q);
+    }
+}
