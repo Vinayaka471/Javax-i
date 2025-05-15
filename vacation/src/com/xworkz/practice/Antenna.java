635c5ef81e684b928522864fee24e73eac9c282b
vinayaka <vinayakah47@gmail.com>
2025-05-15 13:49:22 +0530

    Numbers in series

---
+++ vacation/src/com/xworkz/practice/Antenna.java
@@ -0,0 +1,9 @@
+package com.xworkz.practice;
+
+public class Antenna {
+    public static void main(String[] args){
+        for(int q=200;q<950;q++){
+            System.out.println(q);
+        }
+    }
+}
