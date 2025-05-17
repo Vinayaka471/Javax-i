b851e694cd72296908cfe4afde553826dfd194a3
vinayaka <vinayakah47@gmail.com>
2025-05-17 11:20:05 +0530

    Simple Printing Statement

---
+++ vacation/src/com/xworkz/practice/Poster.java
@@ -0,0 +1,10 @@
+package com.xworkz.practice;
+
+public class Poster {
+    public static void main(String[] args){
+        System.out.println("PES INSTITUTE OF TECHNOLOGY AND MANAGEMENT SHIVAMOGGA");
+        System.out.println("INFORMATION SCIENCE AND ENGINEERING");
+        System.out.println("X-WORKZ, Rajajinagara");
+        System.out.println("Core java, Web technology, MySQL");
+    }
+}
