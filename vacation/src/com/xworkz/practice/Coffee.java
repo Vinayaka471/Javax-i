5601b7d6c043e8630ea1207f872566ab0d236114
vinayaka <vinayakah47@gmail.com>
2025-05-19 22:44:24 +0530

    Concatination

---
+++ vacation/src/com/xworkz/practice/Coffee.java
@@ -0,0 +1,10 @@
+package com.xworkz.practice;
+
+public class Coffee {
+    public static void main(String[] args){
+        String sun="Om";
+        String moon="Namaha";
+        String star="Shivaya";
+        System.out.println(sun+" "+moon+" "+star);
+    }
+}
