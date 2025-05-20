11446c94b9131698aefab737ebe8f537becd2cda
vinayaka <vinayakah47@gmail.com>
2025-05-20 19:51:01 +0530

    Type casting Implicit and Explicit casting

---
+++ vacation/src/com/xworkz/practice/Constructor.java
@@ -0,0 +1,13 @@
+package com.xworkz.practice;
+
+public class Constructor {
+    public static void main(String[] args){
+        int a=540;
+        float b=a;
+        System.out.println(b);
+
+        long k=220000L;
+        byte h=(byte)k;
+        System.out.println(h);
+    }
+}
