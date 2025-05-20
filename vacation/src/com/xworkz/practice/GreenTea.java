4bd7b1fd1eb6d65ec8589d1cfdc2d56d839eab75
vinayaka <vinayakah47@gmail.com>
2025-05-20 07:29:07 +0530

    ChatAt() Method

---
+++ vacation/src/com/xworkz/practice/GreenTea.java
@@ -0,0 +1,15 @@
+package com.xworkz.practice;
+
+public class GreenTea {
+    public static void main(String[] args){
+        String name="Hosanagara";
+        System.out.println(name.charAt(4));
+
+        String man="Vinayaka";
+        System.out.println(man.charAt(5));
+    }
+}
+
+    //CharAt(): The CharAt() method returns the character at the specified index in a string.
+
+                // Java is a o(zero) index based language means(It starts with o(zero) not 1).
