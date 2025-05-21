32ddcea2c7a9b39975ae7aa2f7ad976f1d03fec6
vinayaka <vinayakah47@gmail.com>
2025-05-21 20:33:01 +0530

    Arithmatic Operators

---
+++ Scaler Problems/src/com/xworkz/learn/Mobile.java
@@ -0,0 +1,25 @@
+package com.xworkz.learn;
+
+public class Mobile {
+    public static void main(String[] args){
+        int a=100;
+        int b=6;
+
+        System.out.println(a+b);
+
+
+        System.out.println(a-b);
+
+
+        System.out.println(a*b);
+
+
+        System.out.println(a/b);
+        System.out.println(a/(float)b);
+
+
+        System.out.println(a%b);
+        System.out.println(36%5);
+
+    }
+}
--- Scaler Problems/src/com/xworkz/learn/Tab.java
+++ Scaler Problems/src/com/xworkz/learn/Tab.java
@@ -4,8 +4,8 @@ public class Tab {
     public static void main(String[] args){
         int a=10;
         int b=5;
-        int c=(a*1.0)/b;  // I need to change the int data type into float data type. Otherwise it will get Compilation error
-        System.out.println(c);
+       // int c=(a*1.0)/b;  // I need to change the int data type into float data type. Otherwise it will get Compilation error
+        //System.out.println(c);
     }
 }
 
