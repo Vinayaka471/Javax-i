5ca747e45ffc08eed34135f51bead57a7f02c921
vinayaka <vinayakah47@gmail.com>
2025-05-21 19:55:31 +0530

    Scaler Test Problem Change the Data Type

---
+++ Scaler Problems/src/com/xworkz/learn/Tab.java
@@ -0,0 +1,13 @@
+package com.xworkz.learn;
+
+public class Tab {
+    public static void main(String[] args){
+        int a=10;
+        int b=5;
+        int c=(a*1.0)/b;  // I need to change the int data type into float data type. Otherwise it will get Compilation error
+        System.out.println(c);
+    }
+}
+
+
+             //SCALER 1 TEST 5TH QUESTION (I Make Mistake)
