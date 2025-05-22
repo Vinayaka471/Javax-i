f9dd65429b1ff7eb6ec348fea5f8754310847313
vinayaka <vinayakah47@gmail.com>
2025-05-22 23:50:25 +0530

    Assignment vs Equality Operator

---
+++ Scaler Problems/src/com/xworkz/learn/Notes.java
@@ -0,0 +1,11 @@
+package com.xworkz.learn;
+
+public class Notes {
+    public static void main(String args[]){
+        int x=5;   //x is assigned value 5
+        int y=10;  //y is assigned value 10
+
+        System.out.println(x==5);   //true
+        System.out.println(y==2);   //false
+    }
+}
