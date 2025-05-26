3d493be4e0d5d2884dcc58fac20edb86834a6bc8
vinayaka <vinayakah47@gmail.com>
2025-05-26 23:58:55 +0530

    Print Statement

---
+++ vacation/src/com/xworkz/practice/Table.java
@@ -0,0 +1,9 @@
+package com.xworkz.practice;
+
+public class Table {
+    public static void main(String[] args) {
+        System.out.println("Edustation");
+        System.out.println();
+        System.out.println("Yelahanka");
+    }
+}
