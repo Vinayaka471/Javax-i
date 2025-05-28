fa7b552c37b13e19a91aa6901ae89756d43128bc
vinayaka <vinayakah47@gmail.com>
2025-05-28 20:32:31 +0530

    Multiplying Two Numbers

---
+++ vacation/src/com/xworkz/practice/Drum.java
@@ -0,0 +1,10 @@
+package com.xworkz.practice;
+
+public class Drum {
+    public static void main(String[] args) {
+        int a=100;
+        int b=150;
+        int c=a*b;
+        System.out.println(c);
+    }
+}
