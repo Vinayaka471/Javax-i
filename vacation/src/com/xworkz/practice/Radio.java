a3dc4d1fb4800b9fc768973d1790e9ae26e5ff27
vinayaka <vinayakah47@gmail.com>
2025-05-30 12:43:37 +0530

    ASCII Value

---
+++ vacation/src/com/xworkz/practice/Radio.java
@@ -0,0 +1,14 @@
+package com.xworkz.practice;
+
+public class Radio {
+    public static void main(String[] args) {
+        System.out.println('A'+'B');
+
+        //output: 131
+
+
+        // Based on ASCII Value: A to Z: 65-97  and a to z: 97-122
+
+        //A=65 and B=66
+    }
+}
