696996ee90c54921083ce033747e5ba3f8239f5e
vinayaka <vinayakah47@gmail.com>
2025-06-03 19:31:39 +0530

    If else condition

---
+++ vacation/src/com/xworkz/practice/Mat.java
@@ -0,0 +1,16 @@
+package com.xworkz.practice;
+
+import java.util.Scanner;
+
+public class Mat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x>0){
+            System.out.println("It is positive");
+        }
+        else if(x<0){
+            System.out.println("It is a negative");
+        }
+    }
+}
