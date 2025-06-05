8a6d9e9e27f8b4bd379ecbd147c9325e8f61d7ef
vinayaka <vinayakah47@gmail.com>
2025-06-05 07:27:47 +0530

    If else statement

---
+++ vacation/src/com/xworkz/practice/Slate.java
@@ -0,0 +1,16 @@
+package com.xworkz.practice;
+
+public class Slate {
+    public static void main(String[] args) {
+        String weather = "rainy";
+        if(weather=="rainy"){
+            System.out.println("Take Umbrella");
+        }
+        else if(weather=="Sunny"){
+            System.out.println("Wear Sunglass");
+        }
+        else{
+            System.out.println("Check Weather Forcast");
+        }
+    }
+}
