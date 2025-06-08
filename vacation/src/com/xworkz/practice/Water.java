7dbcb9f4391ed3201b13588491aaa2bbec95f211
vinayaka <vinayakah47@gmail.com>
2025-06-08 13:10:31 +0530

    Printing Hello World without the use of Semicolon

---
+++ vacation/src/com/xworkz/practice/Water.java
@@ -0,0 +1,9 @@
+package com.xworkz.practice;
+
+public class Water {
+    public static void main(String[] args) {
+        if(System.out.printf("Hello World") !=null) {
+        }
+        }
+    }
+
