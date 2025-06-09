026f8a273e339462dcbf97d2f52d0eb98dde8d36
vinayaka <vinayakah47@gmail.com>
2025-06-09 21:41:23 +0530

    Post Increment and Pre Increment

---
+++ vacation/src/com/xworkz/practice/Tree.java
@@ -0,0 +1,9 @@
+package com.xworkz.practice;
+
+public class Tree {
+    public static void main(String[] args) {
+        int v=14;
+        int k=47;
+        System.out.println(v++ + ++k);
+    }
+}
