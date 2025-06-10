3c8f1552882f06b83897d08eb8ccec94d9d2fb0b
vinayaka <vinayakah47@gmail.com>
2025-06-10 07:08:01 +0530

    Variable length in java

---
+++ vacation/src/com/xworkz/practice/Monitor.java
@@ -0,0 +1,8 @@
+package com.xworkz.practice;
+
+public class Monitor {
+    public static void main(String[] args) {
+        String bat="X-WORKZ Rajajinagara Bengaluru";
+        System.out.println(bat.length());
+    }
+}
