168915d5ab136a20309027d53abe7480574fdc19
vinayaka <vinayakah47@gmail.com>
2025-06-10 07:18:19 +0530

    Integer Comparision

---
+++ vacation/src/com/xworkz/practice/Chappal.java
@@ -0,0 +1,9 @@
+package com.xworkz.practice;
+
+public class Chappal {
+    public static void main(String[] args){
+        int a=128;
+        int b=144;
+        System.out.println(a == b);
+    }
+}
