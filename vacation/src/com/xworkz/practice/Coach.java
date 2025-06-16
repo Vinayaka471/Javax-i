645e701efd5a3accac264d070576ec6984acf726
vinayaka <vinayakah47@gmail.com>
2025-06-16 13:40:18 +0530

    Printing Statement Prasadha LEFT

---
+++ vacation/src/com/xworkz/practice/Coach.java
@@ -0,0 +1,7 @@
+package com.xworkz.practice;
+
+public class Coach {
+    public static void main(String[] args) {
+        System.out.println("Going to Bengaluru 16th June 2025 Prasadha(Left)");
+    }
+}
