e20513109b6caa33f79755ff6151f31bdf22b573
vinayaka <vinayakah47@gmail.com>
2025-06-17 08:05:24 +0530

    Printing Statement Internship End

---
+++ vacation/src/com/xworkz/practice/Bottle.java
@@ -0,0 +1,7 @@
+package com.xworkz.practice;
+
+public class Bottle {
+    public static void main(String[] args) {
+        System.out.println("End of the Internship in X-WORKZ June 17th");
+    }
+}
