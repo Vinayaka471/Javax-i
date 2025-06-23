daaf8ea5d855d2d3e6484bfe252c7ad5de0d4f30
vinayaka <vinayakah47@gmail.com>
2025-06-23 14:09:16 +0530

    Break Statement

---
+++ vacation/src/com/xworkz/practice/Paper.java
@@ -0,0 +1,14 @@
+package com.xworkz.practice;
+
+public class Paper {
+    public static void main(String[] args){
+    int i=0;
+    while(true){
+        System.out.println(i);
+        i=i+1;
+        if(i>5) {
+            break;
+        }
+        }
+    }
+}
