11f2f509b149a4e42d9dc19b7daa8ec88df432ba
vinayaka <vinayakah47@gmail.com>
2025-06-25 19:12:44 +0530

    Constant in java Final Keyword

---
+++ vacation/src/com/xworkz/practice/Box.java
@@ -0,0 +1,11 @@
+package com.xworkz.practice;
+
+public class Box {
+    public static void main(String[] args) {
+        final double PI=3.1415926;
+        System.out.println(PI);
+    }
+}
+
+
+//Someone may duplicate the already initialize value and give another value. So make the value constant we use "final"
