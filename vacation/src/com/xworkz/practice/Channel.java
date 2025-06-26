bc0e4916928970224a869140291394b69814aef5
vinayaka <vinayakah47@gmail.com>
2025-06-26 19:13:11 +0530

    Two Dimensional Arrays

--- vacation/src/com/xworkz/practice/Channel.java
+++ vacation/src/com/xworkz/practice/Channel.java
@@ -2,9 +2,9 @@ package com.xworkz.practice;
 
 public class Channel {
     public static void main(String[] args) {
-        int marks[] = {95,97,52};
-        int[] [] finalmarks={{96,93,76}, {67,85,93}};
-        System.out.println(finalmarks[1] [1]);
+        int Marks[] = {95,97,52};
+        int[] [] Finalmarks={{96,93,76}, {67,85,93}};
+        System.out.println(Finalmarks[1] [1]);
     }
 }
  // we select 1,1
