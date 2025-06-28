4eb46dd22c0e0f742faf1e0c5a8f401697d43940
vinayaka <vinayakah47@gmail.com>
2025-06-28 12:09:16 +0530

    Static and Dynamic in java

---
+++ vacation/src/com/xworkz/practice/Notes.java
@@ -0,0 +1,15 @@
+package com.xworkz.practice;
+
+
+// STATIC
+public class Notes {
+    static String COllege="PESITM";
+
+    public static void main(String[] args) {
+        System.out.println(Notes.COllege);
+    }
+}
+
+
+
+
---
+++ vacation/src/com/xworkz/practice/Towel.java
@@ -0,0 +1,12 @@
+package com.xworkz.practice;
+
+public class Towel {
+    String color;
+
+    public static void main(String[] args) {
+        Towel towel=new Towel();
+        towel.color="Teal";
+        System.out.println(towel.color);
+
+    }
+}
