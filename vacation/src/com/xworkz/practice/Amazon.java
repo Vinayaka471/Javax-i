ceabbf837aefe558ada8359d470edc733fa4b33d
vinayaka <vinayakah47@gmail.com>
2025-06-29 19:04:39 +0530

    For Condition in Java

---
+++ vacation/src/com/xworkz/practice/Amazon.java
@@ -0,0 +1,10 @@
+package com.xworkz.practice;
+
+public class Amazon {
+    public static void main(String[] args) {
+        int i=0;
+        for(i=0;i<=10000;i++){
+            System.out.println(i);
+        }
+    }
+}
