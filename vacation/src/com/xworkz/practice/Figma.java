308fd2deb8a28fb8fcd7632bae0e079978ab61fc
vinayaka <vinayakah47@gmail.com>
2025-07-01 21:15:57 +0530

    if, else-if and else Conditional Statements

---
+++ vacation/src/com/xworkz/practice/Figma.java
@@ -0,0 +1,12 @@
+package com.xworkz.practice;
+
+public class Figma {
+    public static void main(String[] args) {
+        int i=100;
+        if(i>=500){
+            System.out.println("The Number is not correct");
+        } else if (i<=100) {
+            System.out.println("The number is right or approximately near to hundred");
+        }
+    }
+}
