4b244111e5c2147e370d863a21d11047acb3ffc7
vinayaka <vinayakah47@gmail.com>
2025-07-04 09:18:59 +0530

    Printing Statement

---
+++ vacation/src/com/xworkz/practice/Button.java
@@ -0,0 +1,8 @@
+package com.xworkz.practice;
+
+public class Button {
+    public static void main(String[] args) {
+        System.out.println("July 4th Wish You Happy Birthday");
+        System.out.println("America Independence day");
+    }
+}
