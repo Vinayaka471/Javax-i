ebe71181c77e0954b21cd5e57d34302d13886722
vinayaka <vinayakah47@gmail.com>
2025-07-05 13:56:07 +0530

    charAt() method

---
+++ vacation/src/com/xworkz/practice/Rim.java
@@ -0,0 +1,11 @@
+package com.xworkz.practice;
+
+public class Rim {
+    public static void main(String[] args) {
+        String rim="Indus 555 TMT";
+        System.out.println(rim.charAt(4));
+    }
+}
+
+
+//charAt: The charAt() method returns the character at the specified index in a string.
