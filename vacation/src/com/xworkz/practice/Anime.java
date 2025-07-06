fc63fc7a49624089d3a4557280dc07cb42f3487a
vinayaka <vinayakah47@gmail.com>
2025-07-06 10:44:37 +0530

    While loop in java

---
+++ vacation/src/com/xworkz/practice/Anime.java
@@ -0,0 +1,11 @@
+package com.xworkz.practice;
+
+public class Anime {
+    public static void main(String[] args) {
+        int i=21;
+        while(i<=2054687){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
