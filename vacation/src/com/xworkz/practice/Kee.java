b5ef5b20584b5a5d3c2e596c908fd8dc6afe8ad6
vinayaka <vinayakah47@gmail.com>
2025-07-08 10:37:28 +0530

    Implicit type casting

---
+++ vacation/src/com/xworkz/practice/Kee.java
@@ -0,0 +1,11 @@
+package com.xworkz.practice;
+
+public class Kee {
+    public static void main(String[] args) {
+        int num=50;
+        double result=num;
+        System.out.println(result);
+    }
+}
+
+// Implicit typecasting: Converts smaller data type into larger data type.
