cb4469815a3b7506dae7685a8db56ca88d79deba
vinayaka <vinayakah47@gmail.com>
2025-07-09 19:47:33 +0530

    Printing Statement

---
+++ vacation/src/com/xworkz/practice/Locker.java
@@ -0,0 +1,10 @@
+package com.xworkz.practice;
+
+import java.util.Scanner;
+
+public class Locker {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        sc.hasNext();
+    }
+}
---
+++ vacation/src/com/xworkz/practice/Mobile.java
@@ -0,0 +1,7 @@
+package com.xworkz.practice;
+
+public class Mobile {
+    public static void main(String[] args) {
+        System.out.println("SAMSUNG 5G A06 6GB 128GB ROM");
+    }
+}
