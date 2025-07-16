c397e5daf0a285575182a96e09d0d63db3907c67
vinayaka <vinayakah47@gmail.com>
2025-07-16 20:23:59 +0530

    Added method to display constructor-like message

---
+++ vacation/src/com/xworkz/practice/Battle.java
@@ -0,0 +1,13 @@
+package com.xworkz.practice;
+
+public class Battle {
+    public void Cap(){
+        System.out.println("No Argument Constructor Running");
+    }
+    public static void main(String[] args) {
+        Battle battle=new Battle();
+        battle.Cap();
+
+
+    }
+}
