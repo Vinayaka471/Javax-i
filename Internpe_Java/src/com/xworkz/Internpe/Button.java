2af937ceb4be9eff98d8481083c129bd50c74db2
vinayaka <vinayakah47@gmail.com>
2025-08-06 21:19:02 +0530

    Data types in java

---
+++ Internpe_Java/src/com/xworkz/Internpe/Button.java
@@ -0,0 +1,10 @@
+package com.xworkz.Internpe;
+
+public class Button {
+
+
+    public static void main(String[] args) {
+        int myfirstnumber=4;
+        System.out.println(myfirstnumber);
+    }
+}
