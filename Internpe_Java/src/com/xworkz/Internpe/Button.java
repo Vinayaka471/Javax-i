266bce5dba0ef07634c011eaa3ee7b1bfc38b035
vinayaka <vinayakah47@gmail.com>
2025-08-06 21:27:45 +0530

    Datatypes in java

--- Internpe_Java/src/com/xworkz/Internpe/Button.java
+++ Internpe_Java/src/com/xworkz/Internpe/Button.java
@@ -6,5 +6,8 @@ public class Button {
     public static void main(String[] args) {
         int myfirstnumber=4;
         System.out.println(myfirstnumber);
+
+        int mysecondnumber=61+52;
+        System.out.println(mysecondnumber);
     }
 }
