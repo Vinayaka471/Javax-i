05ff140e828b430a7f110df47ae4c8f271b01558
vinayaka <vinayakah47@gmail.com>
2025-08-08 10:04:41 +0530

    Bro Code Java

--- Internpe_Java/src/com/xworkz/Internpe/TV9.java
+++ Internpe_Java/src/com/xworkz/Internpe/TV9.java
@@ -1,5 +1,14 @@
 package com.xworkz.Internpe;
 
 public class TV9 {
+    public static void main(String[] args){
+        System.out.print("I Like Java");
+        System.out.print("I Like Web Technology");
+
+
+        System.out.println("===============");
+        System.out.println("I Like Java");
+        System.out.println("I Like Web Technology");
+    }
     
 }
