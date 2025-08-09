7110e20cfdc1935a43438c2edb1c5e50c70137b6
vinayaka <vinayakah47@gmail.com>
2025-08-09 08:22:55 +0530

    Java example for swapping strings using a temporary variable

---
+++ vacation/src/com/xworkz/practice/Setupbox.java
@@ -0,0 +1,9 @@
+package com.xworkz.practice;
+
+public class Setupbox {
+    public static void main(String[] args){
+        int x=123;
+        System.out.println("My Number is: "+x );
+
+    }
+}
