0881a36fc764d2b5f44369a335c0c6816a58b9e1
vinayaka <vinayakah47@gmail.com>
2025-08-09 19:15:43 +0530

    Add user input java example using Scanner class

---
+++ vacation/src/com/xworkz/practice/Terminator.java
@@ -0,0 +1,17 @@
+package com.xworkz.practice;
+import java.util.Scanner;
+
+public class Terminator {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+
+
+        System.out.println("Enter Your Name: ");
+        String name=sc.nextLine();
+
+        System.out.println("Enter Your Age: ");
+        int age=sc.nextInt();
+
+    }
+}
