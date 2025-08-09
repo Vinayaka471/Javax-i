1c15a62c4e5934402481e67ad252ea7cea3ca25b
vinayaka <vinayakah47@gmail.com>
2025-08-09 20:30:26 +0530

    Expression in java= Operands and Operators

---
+++ vacation/src/com/xworkz/practice/Cable.java
@@ -0,0 +1,45 @@
+package com.xworkz.practice;
+
+public class Cable {
+    public static void main(String[] args) {
+        // Expression=Operands and Operators
+
+        // Operands= Numbers,values,quantity,variables.
+
+        // Operators= + - * / %
+
+        int cars =3;
+        cars=cars+1;
+        System.out.println("Cars: "+cars);
+
+        int vans=12;
+        vans++;
+        System.out.println("Vans: "+vans);   // Increment
+
+        int mobiles=2;
+        mobiles = mobiles-1;
+        System.out.println("Mobiles: "+mobiles);
+
+        int charger=3;
+        charger--;  // Decrement
+        System.out.println("Charger :"+charger);
+
+        int bangles=15;
+        bangles=bangles*2;
+        System.out.println("Bangles: "+bangles);
+
+        int TV=4;
+        TV=TV/2;
+        System.out.println("TV: "+TV);
+
+        double Badge=16;
+        Badge=Badge%7;
+        System.out.println("Badge: "+Badge);
+
+        float Tyer=52;
+        Tyer=Tyer/11;
+        System.out.println("Tyer: "+ Tyer);
+
+
+    }
+}
