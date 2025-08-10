e1397960db93fd3f581432c56c815601cdca264b
vinayaka <vinayakah47@gmail.com>
2025-08-10 17:21:00 +0530

    Random values operation in java

---
+++ vacation/src/com/xworkz/practice/Dish.java
@@ -0,0 +1,25 @@
+package com.xworkz.practice;
+import java.util.Random;
+
+public class Dish {
+    public static void main(String[] args) {
+        Random random=new Random();
+
+//        int a=random.nextInt(); It will print random numbers positive or negative without limitations.
+//        System.out.println(a);
+
+//        int b=random.nextInt(8); // It will print 0 to 7
+//        System.out.println(b);
+//
+//        int c=random.nextInt(8)+1; // It will print 0 to 8
+//        System.out.println(c);
+
+//        double k=random.nextDouble();
+//        System.out.println(k);
+        
+        boolean g=random.nextBoolean(); // It will print either true or false
+        System.out.println(g);
+
+
+    }
+}
