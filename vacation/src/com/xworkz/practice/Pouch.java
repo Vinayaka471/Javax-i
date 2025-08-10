2d0b1574bb5deff4f2bfa133fee96a9cccd1cf43
vinayaka <vinayakah47@gmail.com>
2025-08-10 08:19:22 +0530

    Creating small java program using a GUI

--- vacation/src/com/xworkz/practice/Pouch.java
+++ vacation/src/com/xworkz/practice/Pouch.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 public class Pouch {
     public static void main(String[] args){
         String name=JOptionPane.showInputDialog("Enter Your Name");
+        JOptionPane.showMessageDialog(null,"Hello"+name);
 
 
     }
