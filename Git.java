96dadd315976fbfd3ed2e5918eddf33682e9dc52
Vinayaka H <158029421+Vinayaka471@users.noreply.github.com>
2025-08-13 13:36:03 +0530

    X-workz java
    Java assignment

---
+++ Git.java
@@ -0,0 +1,13 @@
+class Git{
+	public static void main(String[] args){
+		
+		
+		System.out.println("git clone repositary url");
+		System.out.println("git add filename");
+		System.out.println("git commit -m "description");
+		System.out.println("git push");
+		System.out.println("git checkout -b branchname");
+		System.out.println("git push -u origin branchname");
+		System.out.println("git push -u origin branchname");
+	}
+}
