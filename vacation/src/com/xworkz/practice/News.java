e27971e398f92f8408d395f51cb6d67b22c71fce
vinayaka <vinayakah47@gmail.com>
2025-08-18 20:59:52 +0530

    Switch case operation in java

---
+++ vacation/src/com/xworkz/practice/News.java
@@ -0,0 +1,23 @@
+package com.xworkz.practice;
+
+public class News {
+    public static void main(String[] args) {
+        String Laptop = "hp";
+
+        switch (Laptop) {
+            case "Asus":
+                System.out.println("Asus Laptop");
+                break;
+            case "Dell":
+                System.out.println("Dell Laptop");
+                break;
+            case "acer":
+                System.out.println("acer Laptop");
+                break;
+            case "hp":
+                System.out.println("hp Laptop");
+                break;
+        }
+    }
+}
+
