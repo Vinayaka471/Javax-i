c3000002b262d14475bfc6cd9db81b9988f37741
vinayaka <vinayakah47@gmail.com>
2025-08-20 20:28:43 +0530

    Operators and operands in java

---
+++ vacation/src/com/xworkz/practice/Guitar.java
@@ -0,0 +1,10 @@
+package com.xworkz.practice;
+
+public class Guitar {
+    public static void main(String[] args) {
+        int h=50;
+        int c=3;
+        int v=h/c;
+        System.out.println(v);
+    }
+}
