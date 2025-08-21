be5a31ff772b5ce52a86b36b08aeb5addcfd8a63
vinayaka <vinayakah47@gmail.com>
2025-08-21 19:10:44 +0530

    Switch case operation in java

---
+++ vacation/src/com/xworkz/practice/SIT.java
@@ -0,0 +1,30 @@
+package com.xworkz.practice;
+public class SIT {
+    public static void main(String[] args) {
+        String place="Siddhapanagudi";
+
+        switch("Siddhppanagudi"){
+            case "Kumbasi":
+                System.out.println("This place is Kumbasi");
+                break;
+            case "Shivamogga":
+                System.out.println("This place is Shivamogga");
+                break;
+            case "Hosanagara":
+                System.out.println("This place is Hosanagara");
+                break;
+            case "Ripponpet":
+                System.out.println("This place is Ripponpet");
+                break;
+            case "Siddhppanagudi":
+                System.out.println("This place is Siddhppanagudi");
+                break;
+            case "Thirthahalli":
+                System.out.println("This place is Thirthahalli");
+                break;
+            case "Bengaluru":
+                System.out.println("This place is Bengaluru");
+                break;
+        }
+    }
+}
