d21ef0feae1b99622e83fade4fe7d1857caf1083
vinayaka <vinayakah47@gmail.com>
2025-08-23 13:11:00 +0530

    String length in java

---
+++ vacation/src/com/xworkz/practice/Window.java
@@ -0,0 +1,8 @@
+package com.xworkz.practice;
+
+public class Window {
+    public static void main(String[] args) {
+        String name="Why this kolavari di";
+        System.out.println(name.length());
+    }
+}
