3101419017a1f7a1eb4adbad8492b24e9e75188b
vinayaka <vinayakah47@gmail.com>
2025-08-24 19:23:52 +0530

    CharAt() in java

---
+++ vacation/src/com/xworkz/practice/Black.java
@@ -0,0 +1,8 @@
+package com.xworkz.practice;
+
+public class Black {
+    public static void main(String[] args) {
+        String sentece="Proud to be Indian";
+        System.out.println(sentece.charAt(7));
+    }
+}
