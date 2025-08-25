63ffbf41fd06af1b48325634b55d7db4738769aa
vinayaka <vinayakah47@gmail.com>
2025-08-25 18:45:00 +0530

    charAt() method and printing statement in java

--- vacation/src/com/xworkz/practice/Black.java
+++ vacation/src/com/xworkz/practice/Black.java
@@ -2,7 +2,7 @@ package com.xworkz.practice;
 
 public class Black {
     public static void main(String[] args) {
-        String sentece="Proud to be Indian";
-        System.out.println(sentece.charAt(7));
+        String sentence="Proud to be Indian";
+        System.out.println(sentence.charAt(7));
     }
 }
---
+++ vacation/src/com/xworkz/practice/Whatsapp.java
@@ -0,0 +1,7 @@
+package com.xworkz.practice;
+
+public class Whatsapp {
+    public static void main(String[] args) {
+        System.out.println("We can connect around 8PM");
+    }
+}
