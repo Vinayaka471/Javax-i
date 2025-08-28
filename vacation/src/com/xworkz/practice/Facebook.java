16061be3f6102729f47b844cc60df964a7685da4
vinayaka <vinayakah47@gmail.com>
2025-08-28 18:58:37 +0530

    Printing Statement

--- vacation/src/com/xworkz/practice/Facebook.java
+++ vacation/src/com/xworkz/practice/Facebook.java
@@ -3,6 +3,7 @@ package com.xworkz.practice;
 public class Facebook {
     public static void main(String[] args) {
         System.out.println("It's done");
+        System.out.println("I am waiting,,,,,,");
     }
 }
    // Printing statement
