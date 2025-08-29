4f989509ecfd98fbd5e7db2f310a9f6be893689c
vinayaka <vinayakah47@gmail.com>
2025-08-29 15:58:40 +0530

    Conditional statement in java

--- vacation/src/com/xworkz/practice/Rainy.java
+++ vacation/src/com/xworkz/practice/Rainy.java
@@ -1,16 +1,33 @@
 package com.xworkz.practice;
+import java.util.Scanner;
 
 public class Rainy {
     public static void main(String[] args) {
-        String weather="Rainy";
+//        String weather="Rainy";
+//
+//        if(weather=="Summer"){
+//            System.out.println("It is a Summer");
+//        } else if (weather=="Spring") {
+//            System.out.println("It is a Spring");
+//        }
+//        else{
+//            System.out.println("It is a Rainy");
+//        }
 
-        if(weather=="Summer"){
-            System.out.println("It is a Summer");
-        } else if (weather=="Spring") {
-            System.out.println("It is a Spring");
+        System.out.println("Enter your number: ");
+        Scanner sc=new Scanner(System.in);
+        int b=sc.nextInt();
+
+
+        if(b>0){
+            System.out.println("It is a positive number");
+        }
+        else if(b<0){
+            System.out.println("It is a negative number");
         }
         else{
-            System.out.println("It is a Rainy");
+            System.out.println("It is a zero");
         }
+
     }
 }
