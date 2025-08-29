01344d5510276e4c4cb0aa2a3b799b834f01a4ac
vinayaka <vinayakah47@gmail.com>
2025-08-29 15:46:15 +0530

    Conditional Statement in java

---
+++ vacation/src/com/xworkz/practice/Rainy.java
@@ -0,0 +1,16 @@
+package com.xworkz.practice;
+
+public class Rainy {
+    public static void main(String[] args) {
+        String weather="Rainy";
+
+        if(weather=="Summer"){
+            System.out.println("It is a Summer");
+        } else if (weather=="Spring") {
+            System.out.println("It is a Spring");
+        }
+        else{
+            System.out.println("It is a Rainy");
+        }
+    }
+}
