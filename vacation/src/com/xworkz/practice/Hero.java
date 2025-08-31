75c9a13f551a43bcc37ec4f1c7cbdefc660c52f8
vinayaka <vinayakah47@gmail.com>
2025-08-31 15:13:04 +0530

    Scanner in java

--- vacation/src/com/xworkz/practice/Hero.java
+++ vacation/src/com/xworkz/practice/Hero.java
@@ -1,7 +1,16 @@
 package com.xworkz.practice;
+import java.util.*;
 
 public class Hero {
     public static void main(String[] args) {
-        System.out.println("Hello Hero");
+   //     System.out.println("Hello Hero");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name=sc.next();
+        System.out.println("Hello "+name);
+
+
+
     }
 }
