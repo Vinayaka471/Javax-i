3cfb52cd6604e23d9a9857fd01747872f723262c
vinayaka <vinayakah47@gmail.com>
2025-09-01 13:44:57 +0530

    Checking whether the number is even or odd

--- vacation/src/com/xworkz/practice/Facebook.java
+++ vacation/src/com/xworkz/practice/Facebook.java
@@ -1,9 +1,20 @@
 package com.xworkz.practice;
+import java.util.Scanner;
 
 public class Facebook {
     public static void main(String[] args) {
-        System.out.println("It's done");
-        System.out.println("I am waiting,,,,,,");
+//        System.out.println("It's done");
+//        System.out.println("I am waiting,,,,,,");
+        // Printing statement
+
+        System.out.println("Enter Your Number: ");
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        if(k % 2==0){               // Modulus(Remainder %)
+            System.out.println("The number is Even");
+        }
+        else{
+            System.out.println("The number is Odd");
+        }
     }
-}
-   // Printing statement
+}   // Checking whether the number is Even or Odd
