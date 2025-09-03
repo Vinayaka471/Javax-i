f829c2963a4977d40405499dd3bd2b9f58ecc18b
vinayaka <vinayakah47@gmail.com>
2025-09-03 16:52:29 +0530

    Enhanced Switch Case in Java

--- vacation/src/com/xworkz/practice/Tiles.java
+++ vacation/src/com/xworkz/practice/Tiles.java
@@ -2,6 +2,24 @@ package com.xworkz.practice;
 
 public class Tiles {
     public static void main(String[] args) {
-        System.out.println("Welcome to Namma Bengaluru");
+//        System.out.println("Welcome to Namma Bengaluru");
+
+        String Laptop = "HP";
+        switch (Laptop) {
+            case "Acer" -> {
+
+                System.out.println("This is Acer Laptop");
+            }
+            case "HP" -> {
+                System.out.println("This is HO Laptop");
+            }
+            case "Asus" -> {
+                System.out.println("This is Asus Laptop");
+
+            }
+        }
     }
 }
+
+
+// Another type of Switch case i.e, Enhacned Switch case
