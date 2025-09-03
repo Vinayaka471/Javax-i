ccce83970cf76839b0084466809c1565e84b4442
vinayaka <vinayakah47@gmail.com>
2025-09-03 18:21:13 +0530

    Finding Vowel and Consonant using Switch case in java

--- vacation/src/com/xworkz/practice/BSNL.java
+++ vacation/src/com/xworkz/practice/BSNL.java
@@ -1,7 +1,32 @@
 package com.xworkz.practice;
+import java.util.Scanner;
 
 public class BSNL {
     public static void main(String[] args) {
-        System.out.println("BSNL: Barath Sanchar Nigam Limited");
+        // System.out.println("BSNL: Barath Sanchar Nigam Limited");
+
+        System.out.println("Enter the Character: ");
+        Scanner sc=new Scanner(System.in);
+
+        char ch= sc.next().charAt(0);
+        switch (ch){
+            case 'a':
+                System.out.println("Vowel");
+                break;
+            case 'e':
+                System.out.println("Vowel");
+                break;
+            case 'i':
+                System.out.println("Vowel");
+                break;
+            case 'o':
+                System.out.println("Vowel");
+                break;
+            case 'u':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonant");
+        }
     }
 }
