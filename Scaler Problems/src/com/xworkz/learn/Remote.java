e693fb63b3e58799dcd3ae164874d3eb244a99d3
vinayaka <vinayakah47@gmail.com>
2025-09-05 10:46:07 +0530

    Logical and operator in java

---
+++ Scaler Problems/src/com/xworkz/learn/Remote.java
@@ -0,0 +1,14 @@
+package com.xworkz.learn;
+
+public class Remote {
+    public static void main(String[] args) {
+        int age=25;
+        String citizenship="Indian";
+        if(age>=18 & citizenship=="Indian"){
+            System.out.println("The person can vote");
+        }
+        else {
+            System.out.println("The person cannot vote");
+        }
+    }
+}
