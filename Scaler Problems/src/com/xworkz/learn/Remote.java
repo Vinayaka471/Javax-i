97d01448deb2b414998a1c294a56b3f00a7bd8be
vinayaka <vinayakah47@gmail.com>
2025-09-05 11:09:13 +0530

    Logical Short Circuit AND &&

--- Scaler Problems/src/com/xworkz/learn/Remote.java
+++ Scaler Problems/src/com/xworkz/learn/Remote.java
@@ -2,13 +2,23 @@ package com.xworkz.learn;
 
 public class Remote {
     public static void main(String[] args) {
-        int age=25;
+//        int age=25;
+//        String citizenship="Indian";
+//        if(age>=18 & citizenship=="Indian"){
+//            System.out.println("The person can vote");
+//        }
+//        else {
+//            System.out.println("The person cannot vote"); // Logical And (&)
+//        }
+
+
+        int age=14;
         String citizenship="Indian";
-        if(age>=18 & citizenship=="Indian"){
+        if(age>=18 && citizenship=="Indian"){
             System.out.println("The person can vote");
         }
-        else {
-            System.out.println("The person cannot vote");
+        else{
+            System.out.println("The person cannot vote"); // Logical Short Circuit And(&&): Only evaluates the second expression if the first expression yields true.
         }
     }
 }
