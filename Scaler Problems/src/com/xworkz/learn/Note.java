c918027b8ecab6e19f156172aaedcec91564c1cd
vinayaka <vinayakah47@gmail.com>
2025-09-06 14:01:38 +0530

    Logical OR operator in java

---
+++ Scaler Problems/src/com/xworkz/learn/Note.java
@@ -0,0 +1,17 @@
+package com.xworkz.learn;
+
+public class Note {
+    public static void main(String[] args) {
+        String PESITM="Information Technology";
+
+        if(PESITM=="Management Studies" | PESITM=="Information Technology") {
+            System.out.println("User Permitted");
+        }
+        else
+        {
+            System.out.println("User Bloked");
+        }
+    }
+}
+
+        // Logical OR Operator
