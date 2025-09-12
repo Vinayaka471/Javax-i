c39fadab07af40ad711656cd8ac9c120d234a7e9
vinayaka <vinayakah47@gmail.com>
2025-09-12 21:17:22 +0530

    Using == to find the given input is true or false

---
+++ Scaler Problems/src/com/xworkz/learn/Row.java
@@ -0,0 +1,9 @@
+package com.xworkz.learn;
+
+public class Row {
+    public static void main(String[] args) {
+        System.out.println(1+1+1+1+1==5);
+    }
+}
+
+// Using == to find the given input is true or false
