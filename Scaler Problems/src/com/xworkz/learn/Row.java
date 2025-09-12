e6b9da82a068fc51ae6b6b9588b79e154bcef8d6
vinayaka <vinayakah47@gmail.com>
2025-09-12 21:56:22 +0530

    java problem to find true or false by using operators and operands

--- Scaler Problems/src/com/xworkz/learn/Row.java
+++ Scaler Problems/src/com/xworkz/learn/Row.java
@@ -2,8 +2,14 @@ package com.xworkz.learn;
 
 public class Row {
     public static void main(String[] args) {
-        System.out.println(1+1+1+1+1==5);
+//        System.out.println(1+1+1+1+1==5);
+//    }
+//}
+//
+// Using == to find the given input is true or false
+
+        int a = 10, b = 20;
+        int c = a/b;
+        System.out.println((a >= 5) && (b <= 10) && (a / c <= 5));
     }
 }
-
-// Using == to find the given input is true or false
