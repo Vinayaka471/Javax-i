58cfac0d5a2ef45cf0f7b75dcc9878f16a9d954a
vinayaka <vinayakah47@gmail.com>
2025-09-14 19:24:34 +0530

    Printing numbers using for loop

---
+++ Scaler Problems/src/com/xworkz/learn/Mirror.java
@@ -0,0 +1,14 @@
+package com.xworkz.learn;
+
+public class Mirror {
+    public static void main(String[] args) {
+        // print the numbers from 1 to 10 by using for loop
+
+        // for(initialization;condition;update){
+    // }
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
