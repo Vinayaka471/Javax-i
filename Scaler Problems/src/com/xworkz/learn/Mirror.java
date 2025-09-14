e65ce3a56965860c075b57dcb7e6980905116dbf
vinayaka <vinayakah47@gmail.com>
2025-09-14 23:56:12 +0530

    Finding Largest number by using For loop

--- Scaler Problems/src/com/xworkz/learn/Mirror.java
+++ Scaler Problems/src/com/xworkz/learn/Mirror.java
@@ -7,28 +7,28 @@ public class Mirror {
         // print the numbers from 1 to 10 by using for loop
 
         // for(initialization;condition;update){
-    // }
+        // }
 //        int i;
 //        for(i=1;i<=10;i++){
 //            System.out.println(i);
 //        }
 
         // finding the largest numbers
-        System.out.println("Enter the number of integer: ");
-        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of iteration: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        int n=sc.nextInt();
-        System.out.println("Enter the numbers: ");
+        int maxValue = Integer.MIN_VALUE;
 
-        int maxValue=Integer.MIN_VALUE;  // lowest value like -infinity
+        System.out.println("Enter your numbers: ");
+        for (int i = 1; i <= n; i++) {
+            int current = sc.nextInt();
 
-    for(int i=1;i<=n;i++) {
-        int current = sc.nextInt();
-
-        maxValue = Math.max(current, maxValue);
-
+            maxValue = Math.max(current, maxValue);
+        }
+        System.out.println("Maximum value is: "+maxValue);
     }
-        System.out.println("Maxvalue :"+maxValue);
-    }
-    }
+}
+
+
 
