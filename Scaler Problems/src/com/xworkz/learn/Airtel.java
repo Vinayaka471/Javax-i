c43e507fcad0de93fe33185c7ce90739b0fcd22a
vinayaka <vinayakah47@gmail.com>
2025-09-15 16:33:00 +0530

    Printing numbers from 1 to 10 using while loop

---
+++ Scaler Problems/src/com/xworkz/learn/Airtel.java
@@ -0,0 +1,14 @@
+package com.xworkz.learn;
+
+public class Airtel {
+    public static void main(String[] args) {
+        // Printing integers from 1 to 10 by using while loop(it executes until it reaches certain conditions)
+
+        int i=1;
+
+        while(i<=10){   //Condition
+            System.out.println(i);
+            i++;  // Update
+        }
+    }
+}
