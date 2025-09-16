dcd067c46fb7e2c3b093f520aabc2425846f5e3e
vinayaka <vinayakah47@gmail.com>
2025-09-16 08:40:41 +0530

    I need to give integer N and count and print it's number

---
+++ Scaler Problems/src/com/xworkz/learn/Charger.java
@@ -0,0 +1,21 @@
+package com.xworkz.learn;
+
+import java.util.Scanner;
+
+public class Charger {
+    public static void main(String[] args) {
+        // I need to give integer N and count and print it's number
+
+        System.out.println("Enter the integers: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int digit = 0;
+
+        while (num > 0) {
+//           num=num/10;
+            num /= 10;
+            digit++;
+        }
+        System.out.println(digit);
+    }
+}
