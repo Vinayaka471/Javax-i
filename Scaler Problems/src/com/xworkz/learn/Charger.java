fb2db25ef247bf7ea9597caa7efd9c10939579f7
vinayaka <vinayakah47@gmail.com>
2025-09-16 09:02:18 +0530

    Give an integer and count print it's number by using while loop and printing the number for 1 to 5 by  using while loop

--- Scaler Problems/src/com/xworkz/learn/Charger.java
+++ Scaler Problems/src/com/xworkz/learn/Charger.java
@@ -3,19 +3,26 @@ package com.xworkz.learn;
 import java.util.Scanner;
 
 public class Charger {
-    public static void main(String[] args) {
-        // I need to give integer N and count and print it's number
-
+   public static void main(String[] args) {
+      // I need to give integer N and count and print it's number
         System.out.println("Enter the integers: ");
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        int digit =0;
+        int digit =1;
 
-        while (num > 0) {
-//           num=num/10;
-            num /= 10;
-            digit++;
-        }
-        System.out.println(digit);
-    }
+       while (num > 0) {
+           num=num/10;
+           num /= 10;
+           digit++;
+       }
+       System.out.println(digit);
+   }
 }
+
+//        int i = 1;
+//        while (i <= 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+//    }
+//}                       Printing numbers from 1 to 5 by using while loop.
