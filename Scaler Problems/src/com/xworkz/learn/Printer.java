79d04b1bb5683f060c3c8b4b763f32c1e471cfcf
vinayaka <vinayakah47@gmail.com>
2025-09-22 07:15:37 +0530

    Printing statement

--- Scaler Problems/src/com/xworkz/learn/Printer.java
+++ Scaler Problems/src/com/xworkz/learn/Printer.java
@@ -4,5 +4,6 @@ public class Printer {
     public static void main(String[] args) {
         System.out.println("Little Progress");
         System.out.println("Searching the real one");
+        System.out.println("Let's start");
     }
 }
