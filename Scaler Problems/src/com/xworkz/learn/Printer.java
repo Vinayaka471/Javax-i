3c83d93d51447675964a45c6e1734bbb68704c41
vinayaka <vinayakah47@gmail.com>
2025-10-07 22:19:59 +0530

    printing statement

--- Scaler Problems/src/com/xworkz/learn/Printer.java
+++ Scaler Problems/src/com/xworkz/learn/Printer.java
@@ -15,5 +15,6 @@ public class Printer {
         System.out.println("Pubg and Freefire tournament app");
         System.out.println("Today is a Sunday");
         System.out.println("Tournament Admin App");
+        System.out.println("Almost done");
     }
 }
