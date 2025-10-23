dad1a022cce2386feeeb7cea7840113fab382934
vinayaka <vinayakah47@gmail.com>
2025-10-23 13:36:43 +0530

    Simple arithmatic operation

--- Scaler Problems/src/com/xworkz/learn/Printer.java
+++ Scaler Problems/src/com/xworkz/learn/Printer.java
@@ -38,9 +38,15 @@ public class Printer {
 //        int d=k*y;
 //        System.out.println(d);
 
-        int d=90;
-        int l=40;
-        int k=d-l;
-        System.out.println(k);
+//        int d=90;
+//        int l=40;
+//        int k=d-l;
+//        System.out.println(k);
+
+        int l=89;
+        int b=64;
+        int s=l/b;
+        System.out.println(s);
+
     }
 }
