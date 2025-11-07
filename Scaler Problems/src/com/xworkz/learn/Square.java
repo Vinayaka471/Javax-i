d47c95055f1c23e10937ca6169d3bcaa3327cd40
vinayaka <vinayakah47@gmail.com>
2025-11-07 14:39:45 +0530

    Add program to print number from 1 to 20 with line break after 5 numbers

--- Scaler Problems/src/com/xworkz/learn/Square.java
+++ Scaler Problems/src/com/xworkz/learn/Square.java
@@ -1,12 +1,23 @@
 package com.xworkz.learn;
 
 public class Square {
-    public static void main(String[] args){
-        for(int i=1;i<=20;i++){
-            System.out.println(i+" ");
-            if(i%5==0){
+    public static void main(String[] args) {
+//        for(int i=1;i<=20;i++){
+//            System.out.println(i+" ");
+//            if(i%5==0){
+//                System.out.println();
+//            }
+//        }
+//    }
+//}
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+            if (i % 5 == 0) {
                 System.out.println();
             }
         }
     }
 }
+
+//Difference is write print and println
