5d879c44e05ccfb24ae18fbccd51ac83f93adf68
vinayaka <vinayakah47@gmail.com>
2025-12-19 16:46:13 +0530

    Printing statement in java

--- Scaler Problems/src/com/xworkz/learn/Printer.java
+++ Scaler Problems/src/com/xworkz/learn/Printer.java
@@ -178,7 +178,9 @@ public class Printer {
         System.out.println("Implemented Content creators google form for KDC Website");
         System.out.println("Implementing KDC_2026 app for free calendar");
         System.out.println("SBDMC_2026 Version 4.15.0+15 version released on 16 December 2025");
-        System.out.println("KDC_2026 app version 2.18.0 released on 17th December 2025");
+        System.out.println("KDC_2026 app version 2.18.0+18 app update got reject due to policy violation because of using government related topic in app description");
+        System.out.println("KDC_2026 app version 2.18.0+18 released on 17th December 2025");
+
 
 
 
