9bbef31d1e1c160d551e802f20db16061d2afa39
vinayaka <vinayakah47@gmail.com>
2026-01-24 20:08:25 +0530

    Dividing two numbers in java

--- Scaler Problems/src/com/xworkz/learn/Mat.java
+++ Scaler Problems/src/com/xworkz/learn/Mat.java
@@ -2,8 +2,13 @@ package com.xworkz.learn;
 
 public class Mat {
     public static void main(String[] args) {
-        int f=127;
-        int j=127;
-        System.out.println(f == j);
+//        int f=127;
+//        int j=127;
+//        System.out.println(f == j);
+
+        int k=180;
+        int x=50;
+        int h=k/x;
+        System.out.println(h);
     }
 }
