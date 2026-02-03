f1d560939196e28eccd047843600fb530057c3ce
vinayaka <vinayakah47@gmail.com>
2026-02-03 20:05:26 +0530

    Arithmetic Operation in java

--- Scaler Problems/src/com/xworkz/learn/Tab.java
+++ Scaler Problems/src/com/xworkz/learn/Tab.java
@@ -2,10 +2,14 @@ package com.xworkz.learn;
 
 public class Tab {
     public static void main(String[] args){
-        int a=10;
-        int b=5;
-       // int c=(a*1.0)/b;  // I need to change the int data type into float data type. Otherwise it will get Compilation error
-        //System.out.println(c);
+//        int a=10;
+//        int b=5;
+//       // int c=(a*1.0)/b;  // I need to change the int data type into float data type. Otherwise it will get Compilation error
+//        //System.out.println(c);
+        int k=46;
+        int s=67;
+        int y=s-k;
+        System.out.println(y);
     }
 }
 
