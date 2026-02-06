e869ca20c28b22cd2eb96f51910ee7f59daaea5f
vinayaka <vinayakah47@gmail.com>
2026-02-06 22:00:15 +0530

    Placeholder in java

---
+++ Scaler Problems/src/com/xworkz/learn/Dodge.java
@@ -0,0 +1,12 @@
+package com.xworkz.learn;
+
+public class Dodge {
+    public static void main(String[] args){
+        String watch="Rolex";
+        int price=20;
+        System.out.printf("My new watch is %s and the watch price is %d",watch,price);
+    }
+}
+
+// Placeholder: It is a symbol or value it holds data temporarily and later replace with a real data
+//note: placeholder doesn't accept the println it accept either System.out.printf() or String.format()
