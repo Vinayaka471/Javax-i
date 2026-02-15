45c5433ca9cf68acfc8b312f1b8059df0751ff04
vinayaka <vinayakah47@gmail.com>
2026-02-15 14:14:09 +0530

    Printing Statement in Java

--- Scaler Problems/src/com/xworkz/learn/Printer.java
+++ Scaler Problems/src/com/xworkz/learn/Printer.java
@@ -206,6 +206,7 @@ public class Printer {
         System.out.println("Completed 100km in hf deluxe pro on 24th january 2026th at hosanagra near kodachadri 09:35 AM college");
         System.out.println("Completed posting approximately 200 Kannada Calendar_2026 Hard Copy on 24-01-2026 Saturday");
         System.out.println("Started to learn car driving in Sharadha Driving School from January 31st 2026 Saturday 4:54 around");
+        System.out.println("Maha Shivarathri on 15th february 2026 Sunday");
 
 
 
