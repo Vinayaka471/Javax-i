31d5e17670a834afc52ad27acfc12a58439980ea
vinayaka <vinayakah47@gmail.com>
2026-02-23 22:21:16 +0530

    Java problem solving

---
+++ Scaler Problems/src/com/xworkz/learn/Hackerrank.java
@@ -0,0 +1,8 @@
+package com.xworkz.learn;
+
+public class Hackerrank {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
